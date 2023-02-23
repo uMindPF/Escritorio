bd486756667364437fa6bef11cbb3c9642f70b6c
Enriclop11 <informenric@gmail.com>
2023-02-23 16:53:44 +0100

    Codigo Mariona
    Co-Authored-By: marywave19 <125997225+marywave19@users.noreply.github.com>

---
+++ PFMariona/AgendaDeCitas/GestionDeCitas.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace uMind.AgendaDeCitas
+{
+    /// <summary>
+    /// Lógica de interacción para GestionDeCitas.xaml
+    /// </summary>
+    public partial class GestionDeCitas : Window
+    {
+        public GestionDeCitas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPerfil_Click(object sender, RoutedEventArgs e)
+        {
+            Perfil perfil = new Perfil();
+            this.Close();
+            perfil.Show();
+        }
+    }
+}
