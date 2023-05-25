3f77eedd47eb9c6ce3436db4271094f5e87ec7a4
Enriclop11 <informenric@gmail.com>
2023-05-25 19:23:27 +0200

    Conexion al tomcat

--- Service/ConnectionInfo.cs
+++ Service/ConnectionInfo.cs
@@ -4,6 +4,6 @@ namespace uMind.Service
 {
     internal class ConnectionInfo
     {
-        public static string URL_API = "http://localhost:8080/";
+        public static string URL_API = "http://212.227.227.190:8080/umind/";
     }
 }
